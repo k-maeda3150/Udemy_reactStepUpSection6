b65684476fea947a24a9d2153034ff6ad0544483
k-maeda3150 <43035825+k-maeda3150@users.noreply.github.com>
2022-06-17 16:55:34 +0900

    43.Recoil

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -3,10 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { SecondaryButton } from "../atom/button/SecondaryButton";
 import { useContext } from "react";
 import { UserContext } from "../../providers/UserProvider";
+import { useSetRecoilState } from "recoil";
+import { userState } from "../../store/userState";
 
 export const Top = () => {
   const navigate = useNavigate();
-  const { setUserInfo } = useContext(UserContext);
+  // const { setUserInfo } = useContext(UserContext);
+  const setUserInfo = useSetRecoilState(userState);
   const onClickAdmin = () => {
     setUserInfo({ isAdmin: true });
     navigate("/users");
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -5,7 +5,8 @@ import { useLocation } from "react-router-dom";
 import { SecondaryButton } from "../atom/button/SecondaryButton";
 import { useContext } from "react";
 import { UserContext } from "../../providers/UserProvider";
-
+import { useRecoilState } from "recoil";
+import { userState } from "../../store/userState";
 const users = [...Array(10).keys()].map((val) => {
   return {
     id: val,
@@ -21,7 +22,9 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { userInfo, setUserInfo } = useContext(UserContext);
+  // const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const [userInfo, setUserInfo] = useRecoilState(userState);
   const onClickSwitch = () => {
     setUserInfo({ isAdmin: !userInfo.isAdmin });
   };
